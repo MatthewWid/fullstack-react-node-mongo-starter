b74c158e109acc5864b729b5f6d504cd517e69fc
MatthewWid <115mattey@gmail.com>
2019-06-23 16:23:34 +1000

    Remove all React components but 'App', 'Notfound' and 'Router'. Add introduction page on start.

--- src/js/components/NotFound.js
+++ src/js/components/NotFound.js
@@ -1,10 +1,5 @@
 import React from "react";
-import {Link} from "react-router-dom";
 
-const NotFound = () => (
-	<>
-		<h1>Page Not Found</h1>
-	</>
-);
+const NotFound = () => (<h1>Page Not Found</h1>);
 
 export default NotFound;
